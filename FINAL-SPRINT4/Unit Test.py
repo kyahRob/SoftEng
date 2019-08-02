ad9cde56547efd57851dfb2e151ec206c826bc8b
Lanz Clinton Valdez <30661906+lanzclintonv@users.noreply.github.com>
2019-08-02 22:47:38 +0800

    Add files via upload

--- FINAL-SPRINT4/Unit Test.py
+++ FINAL-SPRINT4/Unit Test.py
@@ -9,16 +9,16 @@ class Test(unittest.TestCase):
         self.assertFalse(main.CheckPass_Test(""))
         self.assertTrue(main.login_Test("Kate"))
         self.asserTrue(main.CheckPass_Test("123"))
-        self.assertFalse(login_Test("Rico")
-
+        self.assertFalse(login_Test("Rico"))
+                         
     def Signup_Test(self):
         self.assertTrue(Sign.Reg_Test("Gwen C", "gwc", "123", "123"))
         self.assertFalse(Sign.Reg_Test("Gwen C", "gwc", "123", "12345"))
         self.assertFalse(Sign.Reg_Test("", "Jerome", "123", "123"))
                          
     def ImageRecog(self):
-        self.assertTrue(img2txt.recognimage_Test("/lanztest.png")
+        self.assertTrue(img2txt.recognimage_Test("/lanztest.png"))
                         
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
                      
